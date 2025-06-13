4eba0e26faedefd472738dfd4d279295f68bb9b2
woosungking <abuga991008@gmail.com>
2025-06-13 13:28:57 +0900

    프로젝트 최초 커밋

---
+++ src/main/java/com/java/backendus/BackendUsApplication.java
@@ -0,0 +1,13 @@
+package com.java.backendus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUsApplication.class, args);
+	}
+
+}
