25f467ae64624643b864050a62405ee284010c11
woosungking <abuga991008@gmail.com>
2025-06-14 14:42:00 +0900

    env:

--- src/test/java/com/java/backendus/BackendUsApplicationTests.java
+++ src/test/java/com/java/backendus/BackendUsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.java.backendus;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendUsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+// package com.java.backendus;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class BackendUsApplicationTests {
+//
+// 	@Test
+// 	void contextLoads() {
+// 	}
+//
+// }
