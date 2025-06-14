93b448ee870b4a2ab4a277f7159053a4d17080a0
woosungking <abuga991008@gmail.com>
2025-06-14 21:27:16 +0900

    refactor: logging

--- src/main/java/com/java/backendus/domain/member/UserController.java
+++ src/main/java/com/java/backendus/domain/member/UserController.java
@@ -1,5 +1,7 @@
 package com.java.backendus.domain.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,12 +16,14 @@ import com.java.backendus.global.code.RestApiResponse;
 @RestController("api/v1/point_us")
 public class UserController {
 	private final UserService userService;
+	private static Logger log = LoggerFactory.getLogger(UserController.class);
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
 
 	@PostMapping
 	public ResponseEntity<RestApiResponse> savePoint(SavePointReqDto savePointReqDto){
+		log.info("들어오셨나 ?");
 		userService.addUserPoint(savePointReqDto);
 		RestApiResponse restApiResponse = new RestApiResponse(MemberCode.USER_POINT_SAVE_SUCCESS);
 		return ResponseEntity.ok(restApiResponse);
@@ -27,6 +31,7 @@ public class UserController {
 
 	@GetMapping
 	public ResponseEntity<RestApiResponse> checkPoint(@RequestParam("userId")Long userId){
+		log.info("들어오셨나 ?");
 		int point = userService.checkUserPoint(userId);
 		RestApiResponse restApiResponse = new RestApiResponse(MemberCode.USER_POINT_SAVE_SUCCESS, point);
 		return ResponseEntity.ok(restApiResponse);
